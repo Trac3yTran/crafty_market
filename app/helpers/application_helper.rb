c15effc48d04fdf59bf5521c70229863a51bfd0c
Trac3yTran <tran.tracey90@gmail.com>
2020-11-17 02:34:14 +1100

    show page layout updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def show_picture(listing)
+        if listing.picture.attached?
+            return image_tag(listing.picture, {class: 'thumbnail'})
+        else
+            return image_tag("default.png", {class: 'thumbnail'})
+        end
+    end 
+
+
 end
