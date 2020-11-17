0397993f000eee504ae73259bdd2e11b1a4f88e3
Trac3yTran <tran.tracey90@gmail.com>
2020-11-18 01:58:09 +1100

    change background image and resize the display image and show listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
 
-    def show_picture(listing)
-        if listing.picture.attached?
-            return image_tag(listing.picture, {class: 'thumbnail'})
-        else
-            return image_tag("default.png", {class: 'thumbnail'})
-        end
-    end 
-
 
 end
